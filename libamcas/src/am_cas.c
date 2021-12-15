f41d86028a5854ed70c8974a22ef2733eda5784d
yali.zhang <yali.zhang@amlogic.com>
2021-12-15 19:24:58 +0800

    cas-hal: Android p compile.[1/1]
    PD#SWPL-66811
    
    Problem:
    1.DVB modules porting from R to P compile fail.
    
    Solution:
    1.modify it.
    
    Verify:
    verified by T950D4_AM301
    
    Change-Id: I21e51604924e00ca973d90cdeefae614f999ca92
    Signed-off-by: Yali Zhang <yali.zhang@amlogic.com>

--- libamcas/src/am_cas.c
+++ libamcas/src/am_cas.c
@@ -48,7 +48,7 @@ int loadCASLibrary(void)
     DIR *dir = NULL;
     struct dirent *dp = NULL;
     char *path[]={"/product/lib", "/vendor/lib", "/usr/lib"};
-    int i;
+    unsigned int i;
 
     for (i = 0; i < sizeof(path)/sizeof(path[0]); i++) {
 	if (!(dir = opendir(path[i]))) {
