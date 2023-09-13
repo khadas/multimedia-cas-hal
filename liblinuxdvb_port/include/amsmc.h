4312cd21096e4c9d99cd6a0d2be4b55f7a2a0b2e
chuangcheng peng <chuangcheng.peng@amlogic.com>
2023-09-13 14:28:31 +0800

    cas-hal: Update dvb headers [1/1]
    PD#SWPL-139622
    
    Problem:
    update dvb headers
    
    Solution:
    update dvb headers
    
    Verify:
    verified at ohm
    
    Change-Id: I894764de9909b953494d344fefc9c4a9d726b536
    Signed-off-by: chuangcheng peng <chuangcheng.peng@amlogic.com>

--- liblinuxdvb_port/include/amsmc.h
+++ liblinuxdvb_port/include/amsmc.h
@@ -1,11 +1,7 @@
+/* SPDX-License-Identifier: (GPL-2.0+ OR MIT) */
 /*
-* Copyright (c) 2014 Amlogic, Inc. All rights reserved.
-*
-* This source code is subject to the terms and conditions defined in the
-* file 'LICENSE' which is part of this source code package.
-*
-* Description:
-*/
+ * Copyright (c) 2019 Amlogic, Inc. All rights reserved.
+ */
 
 #ifndef _AMSMC_H
 #define _AMSMC_H
@@ -16,32 +12,32 @@
 #define AMSMC_MAX_ATR_LEN    33
 
 enum {
-    AMSMC_CARDOUT = 0,
-    AMSMC_CARDIN = 1
+	AMSMC_CARDOUT = 0,
+	AMSMC_CARDIN = 1
 };
 
 struct am_smc_atr {
-    char    atr[AMSMC_MAX_ATR_LEN];
-    int     atr_len;
+	char    atr[AMSMC_MAX_ATR_LEN];
+	int     atr_len;
 };
 
 struct am_smc_param {
-    int     f;
-    int     d;
-    int     n;
-    int     bwi;
-    int     cwi;
-    int     bgt;
-    int     freq;
-    int     recv_invert;
-    int     recv_lsb_msb;
-    int     recv_no_parity;
-    int     recv_parity;
-    int     xmit_invert;
-    int     xmit_lsb_msb;
-    int     xmit_retries;
-    int     xmit_repeat_dis;
-    int     xmit_parity;
+	int     f;
+	int     d;
+	int     n;
+	int     bwi;
+	int     cwi;
+	int     bgt;
+	int     freq;
+	int     recv_invert;
+	int     recv_lsb_msb;
+	int     recv_no_parity;
+	int     recv_parity;
+	int     xmit_invert;
+	int     xmit_lsb_msb;
+	int     xmit_retries;
+	int     xmit_repeat_dis;
+	int     xmit_parity;
 };
 
 #define AMSMC_IOC_MAGIC  'C'
@@ -52,5 +48,6 @@ struct am_smc_param {
 #define AMSMC_IOC_DEACTIVE     _IO(AMSMC_IOC_MAGIC, 0x03)
 #define AMSMC_IOC_GET_PARAM    _IOR(AMSMC_IOC_MAGIC, 0x04, struct am_smc_param)
 #define AMSMC_IOC_SET_PARAM    _IOW(AMSMC_IOC_MAGIC, 0x05, struct am_smc_param)
+#define AMSMC_IOC_HOT_RESET _IOR(AMSMC_IOC_MAGIC, 0x06, struct am_smc_atr)
 
 #endif
